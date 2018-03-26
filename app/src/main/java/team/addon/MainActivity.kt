38c14eecebffac439285983fe78b355be1addf25
shinying <shinying.lees@gmail.com>
2018-03-26 22:54:58 +0800

    A whole new start

--- app/src/main/java/team/addon/MainActivity.kt
+++ app/src/main/java/team/addon/MainActivity.kt
@@ -1,12 +1,17 @@
 package team.addon
 
+import android.app.PendingIntent.getActivity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ab = supportActionBar
+        ab?.hide()
     }
 }
