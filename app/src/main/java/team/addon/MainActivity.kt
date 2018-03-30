89f21f21ce00a6e8f2a0e583850afc9a62edbc0b
shinying <shinying.lees@gmail.com>
2018-03-30 23:46:23 +0800

    Build first three post-it

--- app/src/main/java/team/addon/MainActivity.kt
+++ app/src/main/java/team/addon/MainActivity.kt
@@ -1,11 +1,15 @@
 package team.addon
 
-import android.app.PendingIntent.getActivity
-import android.content.res.Resources
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+
+import com.mindorks.placeholderview.SwipeDecor
+import com.mindorks.placeholderview.SwipePlaceHolderView
+import com.mindorks.placeholderview.SwipeViewBuilder
+
 import kotlinx.android.synthetic.main.activity_main.*
-import android.graphics.Point
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,12 +19,32 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        val display = windowManager.defaultDisplay
-        val size = Point()
-        display.getSize(size)
-        welcome_postIt.height = size.x - 16
+        dot1.alpha = 0.8F
 
-        dot1.imageAlpha = 192
-        
+        swipeView.getBuilder<SwipePlaceHolderView, SwipeViewBuilder<SwipePlaceHolderView>>()
+                .setDisplayViewCount(5)
+                .setIsUndoEnabled(true)
+                .setSwipeType(SwipePlaceHolderView.SWIPE_TYPE_HORIZONTAL)
+                .setSwipeDecor(SwipeDecor()
+                        .setPaddingLeft(20)
+                        .setRelativeScale(0.01f))
+
+        swipeView.addView(PostIt(swipeView))
+        swipeView.addView(PostIt(swipeView))
+        swipeView.addView(PostIt(swipeView))
+
+        swipeView.addItemRemoveListener {
+            when (it) {
+                2 -> {
+                    dot1.alpha = 0.2F
+                    dot2.alpha = 0.8F
+                    build_own.visibility = View.GONE
+                }
+                1 -> {
+                    dot2.alpha = 0.2F
+                    dot3.alpha = 0.8F
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/team/addon/PostIt.kt
@@ -0,0 +1,52 @@
+package team.addon
+
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import com.mindorks.placeholderview.SwipePlaceHolderView
+import com.mindorks.placeholderview.annotations.Layout
+import com.mindorks.placeholderview.annotations.NonReusable
+import com.mindorks.placeholderview.annotations.Resolve
+import com.mindorks.placeholderview.annotations.View
+import com.mindorks.placeholderview.annotations.swipe.SwipeOut
+import com.mindorks.placeholderview.annotations.swipe.SwipeView
+
+/**
+ * Created by shinying.lee on 2018/3/29.
+ */
+@NonReusable
+@Layout(R.layout.post_it_view)
+class PostIt(swipeView: SwipePlaceHolderView) {
+
+    @View(R.id.post_it)
+    var post_it: ImageView? = null
+
+    @View(R.id.post_content)
+    var post_content: TextView? = null
+
+    @View(R.id.input)
+    var input: EditText? = null
+
+    @SwipeView
+    var view: android.view.View? = null
+
+    protected var mSwipeView: SwipePlaceHolderView = swipeView
+
+    fun setStepHint(dotCur: ImageView, dotNext: ImageView) {
+        dotCur.alpha = 0.2F
+        dotNext.alpha = 0.8F
+    }
+
+    @SwipeOut
+    fun onSwipedOut() {
+        Log.d("DEBUG", "onSwipedOut")
+    }
+
+    @Resolve
+    fun onResolved() {
+        post_content?.setText(R.string.post_text)
+        input?.setHint(R.string.input_hint)
+    }
+
+}
