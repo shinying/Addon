72e0e645b915161be643b6c16f4b5662e803d3da
shinying <shinying.lees@gmail.com>
2018-04-01 23:14:18 +0800

    Make three dots switch

--- app/src/main/java/team/addon/MainActivity.kt
+++ app/src/main/java/team/addon/MainActivity.kt
@@ -1,9 +1,12 @@
 package team.addon
 
+import android.animation.Animator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.ImageView
 
 import com.mindorks.placeholderview.SwipeDecor
 import com.mindorks.placeholderview.SwipePlaceHolderView
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         dot1.alpha = 0.8F
 
+        // build the first three post-it
         swipeView.getBuilder<SwipePlaceHolderView, SwipeViewBuilder<SwipePlaceHolderView>>()
                 .setDisplayViewCount(5)
                 .setIsUndoEnabled(true)
@@ -33,18 +37,42 @@ class MainActivity : AppCompatActivity() {
         swipeView.addView(PostIt(swipeView))
         swipeView.addView(PostIt(swipeView))
 
+        // Initial setting for the step hints
+
+
+
         swipeView.addItemRemoveListener {
             when (it) {
                 2 -> {
-                    dot1.alpha = 0.2F
-                    dot2.alpha = 0.8F
-                    build_own.visibility = View.GONE
+                    switchDot(dot1, dot2)
+                    val anim = AlphaAnimation(1.0f, 0.0f)
+                    anim.duration = 200
+
+                    anim.setAnimationListener(object : Animation.AnimationListener {
+                        override fun onAnimationEnd(animation: Animation?) {
+                            build_own.visibility = View.GONE
+                        }
+                        override fun onAnimationStart(animation: Animation?) { }
+                        override fun onAnimationRepeat(animation: Animation?) { }
+                    })
+
+                    build_own.startAnimation(anim)
+
                 }
                 1 -> {
-                    dot2.alpha = 0.2F
-                    dot3.alpha = 0.8F
+                    switchDot(dot2, dot3)
                 }
             }
         }
+    }
+
+    private fun switchDot(curDot: ImageView, nextDot: ImageView) {
+        curDot.animate()
+                .alpha(0.2F)
+                .duration = 200
+
+        nextDot.animate()
+                .alpha(0.8F)
+                .duration = 200
     }
 }
