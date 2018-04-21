34d8eebc9a29a810f42a2fd01e8f8d46810d8dbc
shinying <shinying.lees@gmail.com>
2018-04-21 11:18:09 +0800

    Fix web connection

--- app/src/main/java/team/addon/EndPoints.kt
+++ app/src/main/java/team/addon/EndPoints.kt
@@ -2,7 +2,7 @@ package team.addon
 
 object EndPoints {
     private val URL_ROOT = "http://postwallapp.herokuapp.com"
-    val wallPin = "$URL_ROOT/postdata.php"
-    val wallName = "$URL_ROOT/"
-    val postIt = "$URL_ROOT/"
+    val joinWall = "$URL_ROOT/postdata.php"
+    val buildWall = "$URL_ROOT/"
+    val stick = "$URL_ROOT/"
 }
