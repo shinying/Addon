c432dfc5c7ac14be7ea439b7af5e3587e1f3d361
Shih Yun Chen <cinnacinna130@gmail.com>
2018-04-25 11:32:54 +0800

    change in draw activity

--- app/src/main/java/team/addon/DrawBoard.kt
+++ app/src/main/java/team/addon/DrawBoard.kt
@@ -1,34 +1,76 @@
-package team.addon
+package devdon.com.painter
+
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import com.mindorks.placeholderview.annotations.View
+import android.view.MotionEvent
+import android.view.View
+import java.io.OutputStream
 
 
+/**
+ * Created by slamdon on 2017/12/13.
+ * invoked by ShihYun from the website on 2018/04/25
+ */
 
-class DrawBoard(context: Context, attrs: AttributeSet) : View {
 
-    protected fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
+class PaintBoard(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private var paint: Paint
+    private var bitmap: Bitmap
+    private var mCanvas: Canvas
 
-        //paint a circle
-        var paint = Paint()
-        paint.setColor(Color.BLUE)
-        paint.setStyle(Paint.Style.STROKE)
-        paint.setStrokeWidth(10)
-        canvas.drawCircle(120, 80, 60, paint)
+    private var startX:Float = 0f
+    private var startY:Float = 0f
 
-        //paint string
-        paint = Paint()
-        paint.setColor(Color.YELLOW)
-        paint.setTextSize(20)
-        canvas.drawText("My name is Linc!", 245, 140, paint)
+    init {
+        // bitmap
+        val width = Resources.getSystem().displayMetrics.widthPixels
+        bitmap = Bitmap.createBitmap(width, 800, Bitmap.Config.ARGB_8888)
 
-        //draw line
+        // Canvas
+        mCanvas = Canvas(bitmap)
+        mCanvas.drawColor(Color.GRAY)
+
+        // Paint
         paint = Paint()
         paint.setColor(Color.BLACK)
-        canvas.drawLine(245, 145, 500, 145, paint)
+        paint.setStrokeWidth(10f)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas!!.drawBitmap(bitmap, 0f, 0f, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> {
+                startX = event.x
+                startY = event.y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val stopX = event.x
+                val stopY = event.y
+
+                mCanvas.drawLine(startX, startY, stopX, stopY, paint)
+                startX = event.x
+                startY = event.y
+
+                // call onDraw
+                invalidate()
+            }
+        }
+
+        return true
+    }
+
+    fun saveBitmap(stream: OutputStream) {
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
     }
 }
