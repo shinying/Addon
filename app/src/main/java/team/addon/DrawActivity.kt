85b4383fb01981d5ad9016d2da0b253e7d451e44
shinying <shinying.lees@gmail.com>
2018-04-29 11:21:08 +0800

    Add animation and eraser

--- app/src/main/java/team/addon/DrawActivity.kt
+++ app/src/main/java/team/addon/DrawActivity.kt
@@ -1,31 +1,18 @@
 package team.addon
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.*
-import android.net.Uri
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
-import android.os.Environment
-import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.util.TypedValue
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
+import android.widget.ImageButton
 import com.mindorks.placeholderview.SwipeDecor
 import com.mindorks.placeholderview.SwipePlaceHolderView
 import com.mindorks.placeholderview.SwipeViewBuilder
 import kotlinx.android.synthetic.main.activity_draw.*
-import android.os.Parcelable
-import devdon.com.painter.PaintBoard
-import android.graphics.Bitmap.CompressFormat
-import android.opengl.Visibility
-import android.support.v4.content.ContextCompat
-import android.util.LruCache
-import android.view.View
-import android.widget.*
-import kotlinx.android.synthetic.main.activity_draw.view.*
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
 
 class DrawActivity : AppCompatActivity() {
 
@@ -33,15 +20,19 @@ class DrawActivity : AppCompatActivity() {
 
     private var step = 0
 
-    private val smallColor = 30 * resources.displayMetrics.density.toInt()
-
-    private val bigColor = 50 * resources.displayMetrics.density.toInt()
-
-    private lateinit var curPaintColor: Button
-
-    private lateinit var paintBoard: PaintBoard
+    private lateinit var curPaintColor: ImageButton
 
     private lateinit var tempCanvas: Bitmap
+
+    private var erasing = false
+
+    private var colorBfErase = 0
+
+    private var widthBfErase = 0F
+
+    private var sent = false
+
+    private lateinit var postIt2Stick: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,42 +70,98 @@ class DrawActivity : AppCompatActivity() {
 
                     canvas.clearCanvas()
                     canvas.hasContent = false
+
+                    sent = false
                 }
             }
         }
 
-        // handle paintBoard
-        paintBoard = canvas
-        paintBoard.setPaint()
-
-
-        // handle clicking
-        brush.setOnClickListener {
-            tool_bar.visibility = View.GONE
-            draw_bar.visibility = View.VISIBLE
-            canvas.visibility = View.VISIBLE
-            paint_color.visibility = View.VISIBLE
-            swipeView_d.disableTouchSwipe()
-
-            if(canvas.hasContent){
-                tempCanvas = getCanvasCache(canvas)
+        swipeView_d.addItemRemoveListener {
+            if(!sent) {
+                postIt2Stick = getCanvasCache(canvas)
+                sent = true
             }
         }
 
+        // handle paintBoard
+        canvas.setPaint()
+
+        // set paint color
+        curPaintColor = paint1
+
+        val colorDot = arrayOf(paint1, paint2, paint3, paint4, paint5, paint6, paint7, paint8)
+        val colorSet = arrayOf(R.color.paint_black, R.color.paint_white, R.color.paint_gray, R.color.paint_red, R.color.paint_brown, R.color.paint_green, R.color.paint_blue, R.color.paint_purple)
+
+        val smallPadding = toPx(12F)
+        val bigPadding = toPx(5F)
+
+        for(i in 0..7) {
+            colorDot[i].setOnClickListener {
+
+                if (curPaintColor != colorDot[i]) {
+                    canvas.setPaintColor(colorSet[i])
+
+                    // make small the last using color
+                    curPaintColor.setPadding(smallPadding, smallPadding, smallPadding, smallPadding )
+                    curPaintColor.animate().scaleXBy(-0.3F).scaleYBy(-0.3F)
+
+                    // make big the chosen color
+                    colorDot[i].setPadding(bigPadding, bigPadding, bigPadding, bigPadding)
+                    colorDot[i].animate().scaleXBy(0.3F).scaleYBy(0.3F)
+                    curPaintColor = colorDot[i]
+                }
+            }
+        }
+
+
+        // handle clicking event
+        brush.setOnClickListener {
+
+            // set up UI
+            tool_bar.visibility = View.GONE
+            draw_bar.visibility = View.VISIBLE
+            canvas.visibility = View.VISIBLE
+            paint_palette.visibility = View.VISIBLE
+            swipeView_d.disableTouchSwipe()
+
+            // capture current post-it to canvas
+            if(canvas.hasContent){
+                tempCanvas = getCanvasCache(canvas)
+            }
+            if(curPaintColor == paint1){
+                paint1.animate().scaleXBy(0.3F).scaleYBy(0.3F)
+                paint1.setPadding(bigPadding, bigPadding, bigPadding, bigPadding)
+            }
+        }
+
+
         done.setOnClickListener {
 
+            // capture current canvas to post_it
             postPile[curPost].frame.setImageBitmap(getCanvasCache(canvas))
 
+            // set up UI
             step += 1
             draw_bar.visibility = View.GONE
             tool_bar.visibility = View.VISIBLE
             canvas.visibility = View.GONE
+            paint_palette.visibility = View.GONE
             swipeView_d.enableTouchSwipe()
-
             canvas.hasContent = true
+
+            // initialize eraser
+            if (erasing) {
+                eraser.setImageDrawable(resources.getDrawable(R.drawable.ic_eraser, theme))
+                canvas.setPaintColor(colorBfErase)
+                canvas.setPaintWidth(widthBfErase)
+                erasing = false
+            }
         }
 
+
         clear.setOnClickListener {
+
+            // capture current post-it to canvas
             if(canvas.hasContent){
                 postPile[curPost].frame.setImageBitmap(tempCanvas)
                 canvas.setBitmap(tempCanvas)
@@ -123,51 +170,56 @@ class DrawActivity : AppCompatActivity() {
                 canvas.clearCanvas()
             }
 
+            // set up UI
             draw_bar.visibility = View.GONE
             tool_bar.visibility = View.VISIBLE
             canvas.visibility = View.GONE
-            paint_color.visibility = View.GONE
+            paint_palette.visibility = View.GONE
             swipeView_d.enableTouchSwipe()
+
+            // initialize eraser
+            if (erasing) {
+                eraser.setImageDrawable(resources.getDrawable(R.drawable.ic_eraser, theme))
+                canvas.setPaintColor(colorBfErase)
+                canvas.setPaintWidth(widthBfErase)
+                erasing = false
+            }
         }
 
-        // set paint color
-        curPaintColor = paint1
+        eraser.setOnClickListener {
 
-        paint1.setOnClickListener {
-            canvas.setPaintColor(R.color.paint_black)
-            changePaintColor(curPaintColor, paint1)
-        }
-        paint2.setOnClickListener {
-            canvas.setPaintColor(R.color.paint_white)
-            changePaintColor(curPaintColor, paint2)
-        }
-        paint3.setOnClickListener {
-            canvas.setPaintColor(R.color.paint_gray)
-            changePaintColor(curPaintColor, paint3)
-        }
-        paint4.setOnClickListener {
-            canvas.setPaintColor(R.color.paint_red)
-            changePaintColor(curPaintColor, paint4)
-        }
-        paint5.setOnClickListener {
-            canvas.setPaintColor(R.color.paint_brown)
-            changePaintColor(curPaintColor, paint5)
-        }
-        paint6.setOnClickListener {
-            canvas.setPaintColor(R.color.paint_green)
-            changePaintColor(curPaintColor, paint6)
-        }
-        paint7.setOnClickListener {
-            canvas.setPaintColor(R.color.paint_blue)
-            changePaintColor(curPaintColor, paint7)
-        }
-        paint8.setOnClickListener {
-            canvas.setPaintColor(R.color.paint_purple)
-            changePaintColor(curPaintColor, paint8)
+            // back to drawing
+            if(erasing) {
+                eraser.setImageDrawable(resources.getDrawable(R.drawable.ic_eraser, theme))
+                paint_palette.visibility = View.VISIBLE
+                canvas.setPaintColor(colorBfErase)
+                canvas.setPaintWidth(widthBfErase)
+                erasing = false
+            }
+
+            // choose the color of background and start to erase
+            else {
+                colorBfErase = when (curPaintColor) {
+                    paint2 -> colorSet[1]
+                    paint3 -> colorSet[2]
+                    paint4 -> colorSet[3]
+                    paint5 -> colorSet[4]
+                    paint6 -> colorSet[5]
+                    paint7 -> colorSet[6]
+                    paint8 -> colorSet[7]
+                    else -> colorSet[0]
+                }
+                widthBfErase = canvas.paintWidth()
+                canvas.setPaintColor(canvas.bgColor)
+                canvas.setPaintWidth(80F)
+                paint_palette.visibility = View.GONE
+                eraser.setImageDrawable(resources.getDrawable(R.drawable.ic_eraser_on, theme))
+                erasing = true
+            }
         }
     }
 
-
+    // take a screen shot of a view
     private fun getCanvasCache(view: View): Bitmap {
 
         val bitmap = Bitmap.createBitmap(view.width, view.height,
@@ -182,14 +234,9 @@ class DrawActivity : AppCompatActivity() {
         return bitmap
     }
 
-    private fun changePaintColor(lastColor: Button, newColor: Button) {
-
-        lastColor.width = smallColor
-        lastColor.height = smallColor
-
-        curPaintColor = newColor
-
-        newColor.width = bigColor
-        newColor.width = bigColor
+    // turn dp to pixel
+    private fun toPx(dp: Float): Int {
+        val metrics = applicationContext.resources.displayMetrics
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, metrics).toInt()
     }
 }
